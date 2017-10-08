9c16221734a402c6dc023a62415cedcd9bafe53e
Alexey Popov <alexey.popov@umbrella-web.com>
2017-10-08 23:16:19 +0300

    created basic redux component

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { clear, loadStories } from '../actions/index';
 
-class Stories extends React.Component {
-  render() {
+function Stories(props) {
+  return (
+    <div>
+      <button type="button" onClick={props.loadStories}>Loadd top 3 stories</button>
+      <button type="button" onClick={props.clear}>Clear</button>
+      <StoryList {...props} />
+    </div>
+  );
+}
+
+function StoryList(props) {
+  if (props.items.length === 0) return null;
+  else {
     return (
-      <pre><code>{JSON.stringify(this.props, null, 2)}</code></pre>
+      <div>
+        {props.items.map((item) => <Story {...item} key={item.id} />)}
+      </div>
     );
   }
+}
+
+function Story(props) {
+  return <p>{props.title}</p>
 }
 
 function mapState(state) {
   return state;
 }
 
-export default connect(mapState)(Stories);
+function mapDispatch(dispatch) {
+  return {
+    loadStories: () => dispatch(loadStories()),
+    clear: () => dispatch(clear()),
+  }
+}
+
+export default connect(mapState, mapDispatch)(Stories);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,50 @@
+import { CLEAR_STORIES, LOAD_STORIES } from '../actions/index';
+
+const stories = [
+  {
+    by: 'test by',
+    id: 111111,
+    title: 'test title 1',
+    url: 'http://test.com'
+  },
+  {
+    by: 'test by',
+    id: 22222,
+    title: 'test title 2',
+    url: 'http://test.com'
+  },
+  {
+    by: 'test by',
+    id: 33333,
+    title: 'test title 3',
+    url: 'http://test.com'
+  },
+  {
+    by: 'test by',
+    id: 4444,
+    title: 'test title 4',
+    url: 'http://test.com'
+  }
+];
+
 const initialState = {
   items: [],
 };
 
-export function storedReducers(state = initialState, action) {
-  return state;
+export function storiesReducers(state = initialState, action) {
+  switch (action.type) {
+    case LOAD_STORIES:
+      return {
+        items: stories.slice(),
+      };
+    case CLEAR_STORIES:
+      return {
+        items: [],
+      };
+    default:
+      return state;
+  }
 }
 
 
-export default storedReducers;
+export default storiesReducers;
