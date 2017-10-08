83c85a4d25a2231ed3c77010cff5cfe2bbd84503
Alexey Popov <alexey.popov@umbrella-web.com>
2017-10-08 23:45:58 +0300

    added dev-tools

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import { createEpicMiddleware } from 'redux-observable'
 import reducer from './reducers';
@@ -10,7 +10,13 @@ import registerServiceWorker from './registerServiceWorker';
 import { rootEpic } from './epics/index';
 
 const epicMiddleware = createEpicMiddleware(rootEpic);
-const store = createStore(reducer, applyMiddleware(epicMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducer,
+  composeEnhancers(
+    applyMiddleware(epicMiddleware)
+  ),
+);
 
 ReactDOM.render(
   <Provider store={store}>
